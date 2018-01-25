130a9ac63022a43638130e0899e05521d90cf6d2
Matthew Aderhold <matthewaderhold@gmail.com>
2018-01-25 09:25:21 -0600

    Removed unused imports that were previously  commented out

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import NewChirp from "./components/NewChirp";
-// import Welcome from "./components/Welcome";
-// import ChirpForm from "./components/ChirpForm";
-// import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import SideBarLeft from "./components/SideBarLeft"
 import SideBarRight from "./components/SideBarRight"
